2def5971808d62535a8f7de2d374df51238e65c2
BingNa Yang <bingnayang@gmail.com>
2020-06-13 09:14:04 -0400

    add convert position method and add color to X and O

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_CYAN = "\u001B[36m";
     static boolean setPlayer;
+    static List<Integer> humanMarks = new ArrayList<>();
+    static List<Integer> robotMarks = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,31 +21,55 @@ public class Main {
         System.out.println("Enter Starting PLayer (1 for Human or 2 for Robot): ");
         int startingPlayer = scanner.nextInt();
 
-        if(startingPlayer == 1){
+        if (startingPlayer == 1) {
             setPlayer = true;
         }
 
-        while(true){
-
-            if(setPlayer == true){
-                System.out.println("Human enter your position: ");
+        while (true) {
+            if (setPlayer == true) {
+                System.out.print("Human enter your position " + ANSI_RED + "[X]: " + ANSI_RESET);
                 ttt.changePlayer("human");
-                int humanPlayer1 = scanner.nextInt();
-                int humanPlayer2 = scanner.nextInt();
-                ttt.placeMark(humanPlayer1,humanPlayer2);
+                int humanPlayer = scanner.nextInt();
+                // Check if the spot is already marked
+                if (humanMarks.size() == 0) {
+                    humanMarks.add(humanPlayer);
+                }else{
+                    while (humanMarks.contains(humanPlayer) || robotMarks.contains(humanPlayer)) {
+                        System.out.println("Spot has been marked please reenter your position: ");
+                        humanPlayer = scanner.nextInt();
+                        humanMarks.add(humanPlayer);
+                    }
+                }
+
+                // Convert player position to game board position
+                ttt.convertPosition(humanPlayer);
                 ttt.printGameBoard();
+                // For testing
+                System.out.println("HumanMarks size(): " + humanMarks.size());
+                System.out.println("Human player marks position: " + humanMarks.toString());
                 setPlayer = false;
-            }else{
-                System.out.println("Robot position: ");
+            } else {
+                System.out.print("Robot position " + ANSI_CYAN + "[O]: " + ANSI_RESET);
                 ttt.changePlayer("cpu");
-                int robotPlayer1 = scanner.nextInt();
-                int robotPlayer2 = scanner.nextInt();
-                ttt.placeMark(robotPlayer1,robotPlayer2);
+                int robotPlayer = scanner.nextInt();
+                // Check if the spot is already marked
+//                if(robotMarks.size() > 0){
+//                    while(robotMarks.contains(robotPlayer) || humanMarks.contains(robotPlayer)){
+//                        System.out.println("Spot has been marked please reenter your position: ");
+//                        robotPlayer = scanner.nextInt();
+//                        robotMarks.add(robotPlayer);
+//                    }
+//                }else {
+//                    robotMarks.add(robotPlayer);
+//                }
+                // Convert player position to game board position
+                ttt.convertPosition(robotPlayer);
                 ttt.printGameBoard();
+                // For testing
+                System.out.println("Robot player marks position: " + robotMarks.toString());
                 setPlayer = true;
             }
-
-            if(startingPlayer == 0){
+            if (startingPlayer == 0) {
                 break;
             }
         }
--- src/com/company/TicTacToe.java
+++ src/com/company/TicTacToe.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class TicTacToe {
+//    {{1,2,3},{4,5,6},{7,8,9},{1,5,9},{3,5,7}};
+
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_CYAN = "\u001B[36m";
@@ -44,6 +48,38 @@ public class TicTacToe {
             currentPlayer = ANSI_CYAN+"O"+ANSI_RESET;
         }
         return currentPlayer;
+    }
+    public void convertPosition(int positionConvert){
+        switch (positionConvert){
+            case 1:
+                placeMark(0,1);
+                break;
+            case 2:
+                placeMark(0,3);
+                break;
+            case 3:
+                placeMark(0,5);
+                break;
+            case 4:
+                placeMark(2,1);
+                break;
+            case 5:
+                placeMark(2,3);
+                break;
+            case 6:
+                placeMark(2,5);
+                break;
+            case 7:
+                placeMark(4,1);
+                break;
+            case 8:
+                placeMark(4,3);
+                break;
+            case 9:
+                placeMark(4,5);
+                break;
+        }
+
     }
 
 }
