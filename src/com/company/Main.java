a5ef883cd492a23b296cc5f7202e9333b98b0a20
BingNa Yang <bingnayang@gmail.com>
2020-06-12 20:02:37 -0400

    create generate random integer method for cpu player

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         TicTacToe ttt = new TicTacToe();
         ttt.printGameBoard();
-        ttt.placeMark(0,1);
+
+        System.out.println("Enter Starting PLayer (Human or Computer): ");
+
+        ttt.changePlayer("human");
+
+
+        ttt.placeMark(2,3);
 
         ttt.printGameBoard();
     }
--- src/com/company/TicTacToe.java
+++ src/com/company/TicTacToe.java
@@ -1,31 +1,45 @@
 package com.company;
 
+import java.util.Random;
+
 public class TicTacToe {
     private String currentPlayer = "X";
     private String[][] tttGameBoard = {
             {"| ","1"," | ","2"," | ","3"," |"},
-            {"| ","-"," + ","-"," + ","-"," |"},
+            {"+ ","-"," + ","-"," + ","-"," +"},
             {"| ","4"," | ","5"," | ","6"," |"},
-            {"| ","-"," + ","-"," + ","-"," |"},
+            {"+ ","-"," + ","-"," + ","-"," +"},
             {"| ","7"," | ","8"," | ","9"," |"}};
 
-    public TicTacToe() {
-
-    }
-
     public void printGameBoard(){
-        System.out.println("+-----------+");
+        System.out.println("+---+---+---+");
         for(String[] row: tttGameBoard){
             for(String col : row){
                 System.out.print(col);
             }
             System.out.println();
         }
-        System.out.println("+-----------+");
+        System.out.println("+---+---+---+");
         System.out.println();
     }
     public boolean placeMark(int row, int col){
         tttGameBoard[row][col] = currentPlayer;
         return true;
     }
+
+    public int cpuPosition(){
+        Random random = new Random();
+        int randPickedPosition = random.nextInt(9)+1;
+        return randPickedPosition;
+    }
+
+    public String changePlayer(String player){
+        if(player == "human"){
+            currentPlayer = "X";
+        }else{
+            currentPlayer = "O";
+        }
+        return currentPlayer;
+    }
+
 }
